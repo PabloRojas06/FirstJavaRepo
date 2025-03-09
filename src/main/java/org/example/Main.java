44963a6211f4e3f49b01f7c9c3a7bd0611979ff7
PabloRojas06 <pablorojas0620@gmail.com>
2025-03-08 19:20:04 -0600

    New prinln for practice, update project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hola, es mi primera vez usando github!");
+        System.out.println("Hola, es mi primera vez usando github!\n\n");
+        System.out.println("Este es un nuevo mensaje para prácticar!");
 
     }
 }
